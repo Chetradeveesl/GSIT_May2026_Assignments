d4e8f61500e86a21b2c1f676db880cb5f9ad1337
chetradeveesl <notifyme.2496@gmail.com>
2026-07-01 19:41:30 +0530

    Daily assignment using while loop

---
+++ Daily_Assignments/daily_assignments/src/day5_30_06_2026/Answer1_AlternateUpperCaseLetter.java
@@ -0,0 +1,15 @@
+package day5_30_06_2026;
+
+public class Answer1_AlternateUpperCaseLetter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=65;
+		while(i<=90)
+		{
+			System.out.print((char)i+" ");
+			i+=2;		
+		}
+	}
+
+}
